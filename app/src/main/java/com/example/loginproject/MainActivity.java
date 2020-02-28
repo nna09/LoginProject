6b4d1320d3cfa4d5f866bf23387c76f2e94b6f5c
Nowshin Nawar Arony <n.nawar94@yahoo.com>
2020-02-28 22:57:36 +0600

    Passing Values using Intent

--- app/src/main/java/com/example/loginproject/MainActivity.java
+++ app/src/main/java/com/example/loginproject/MainActivity.java
@@ -6,23 +6,34 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     Button enterNewActivityButton;
-
+    EditText sendUserIDText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         enterNewActivityButton = (Button) findViewById(R.id.loginButton);
+        sendUserIDText = (EditText) findViewById(R.id.userIdText);
+
         enterNewActivityButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
+
+        String userIDStr = sendUserIDText.getText().toString();
+
         Intent loginIntent = new Intent(MainActivity.this, SecondActivity.class);
+
+        loginIntent.putExtra("name","ABCD");
+        loginIntent.putExtra("studentId","1234567");
+        loginIntent.putExtra("user",userIDStr);
+
         startActivity(loginIntent);
     }
 }
--- app/src/main/java/com/example/loginproject/SecondActivity.java
+++ app/src/main/java/com/example/loginproject/SecondActivity.java
@@ -6,11 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity implements View.OnClickListener{
 
     Button backToMainActivityButton;
+    TextView nameTextView, studentIDTextView, userIDTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +22,24 @@ public class SecondActivity extends AppCompatActivity implements View.OnClickLis
 
         backToMainActivityButton = (Button) findViewById(R.id.logoutButton);
         backToMainActivityButton.setOnClickListener(this);
+
+        nameTextView = (TextView) findViewById(R.id.name);
+        studentIDTextView = (TextView) findViewById(R.id.StudentID);
+        userIDTextView = (TextView) findViewById(R.id.userIDShow);
+
+        Bundle bundle = getIntent().getExtras();
+
+        if(bundle!=null){
+
+            String name = bundle.getString("name");
+            String studentID = bundle.getString("studentId");
+            String userID = bundle.getString("user");
+
+            nameTextView.setText(name);
+            studentIDTextView.setText(studentID);
+            userIDTextView.setText(userID);
+
+        }
 
     }
 
